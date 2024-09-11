57cf22c1abd1432b33c6610482d76656491a2e3b
shirayoru <130097435+shirayoru@users.noreply.github.com>
2024-09-12 00:25:39 +0800

    modified web-server in 9/12/2024

--- crates/web-server/src/main.rs
+++ crates/web-server/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use axum::{response::IntoResponse, routing::get, Router};
+use git2::{Repository};
+
+#[tokio::main]
+async fn main() {
+    let routers = Router::new()
+        .route("/hello", get(handler_hello));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    println!("Listening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, routers).await.unwrap();
+    clone_repository().await.unwrap();
+}
+
+async fn handler_hello() -> impl IntoResponse {
+    "Hello, world!"
+}
+
+
+async fn clone_repository() -> Result<(), Box<dyn Error>> {
+    let dst = "/c/Users/shirayoru/Desktop/tmp/repos/";
+    tokio::fs::create_dir_all(dst).await?;
+    let repo_url = "git@github.com:Byron/gitoxide.git";
+
+    let repo = Repository::clone(repo_url, dst)?;
+    
+    Ok(())
 }
